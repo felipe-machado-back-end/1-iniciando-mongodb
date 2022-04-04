703e9fb06eb29ae259b0e3a2d544ef7ea19ecc8f
Felipe Machado <felipe.machado.back.end@gmail.com>
2022-04-04 20:22:57 -0300

    atualizando varios campos utilizando o updateMany ou updateOne que funcionam da mesma forma porem um atualiza um e o outro varios

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -4,14 +4,9 @@ import { Product } from "../models/Product";
 import User from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
-  /*SALVANDO USANDO O SAVE */
-  let newUser = new User();
-  newUser.name = { firstName: "Andre", lastName: "Soares" };
-  newUser.email = "andre@hotmail.com";
-  newUser.age = 35;
-  newUser.interests = ["programação", "skate"];
-  let resultado = await newUser.save();
-  console.log("NOVO USUÁRIO", resultado);
+  //utilizando updateMany
+
+  await User.updateMany({ age: { $lte: 18 } }, { age: 25 });
 
   let age: number = 90;
   let showOld: boolean = false;
