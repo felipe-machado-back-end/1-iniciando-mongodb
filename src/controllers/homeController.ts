65e92c681f2b854827f590719390c3132e50a3fa
Felipe Machado <felipe.machado.back.end@gmail.com>
2022-04-03 22:06:47 -0300

    Criação de três diferentes filtros do mongodb find,findOne e findId

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -4,7 +4,41 @@ import { Product } from "../models/Product";
 import User from "../models/User";
 
 export const home = async (req: Request, res: Response) => {
-  let usuarios = await User.find({});
+  /*
+  
+  acha o registro por uma campo especifico
+   let usuarios = await User.findOne({
+    email: "felipe.email.com",
+  });
+  */
+  /**  ACHA PELO ID  UM CAMPO APENAS
+   let usuarios = await User.findById("6248e442a0762cf7a0e45de4");
+  */
+  //* find retorna todos registros  NÃO APENAS UM ,exemplo retornando um dado dentro de um objeto
+
+  /** 
+    let usuarios = await User.find({
+    "name.primeiroNome": "Paulo",
+  });
+  */
+
+  // pegando um campo dentro de um array
+  /*
+    let usuarios = await User.find({
+    interests: "Programação",
+  });
+
+  */
+  /*
+  gt = maior que 
+  gte = maior ou igual 
+  lt = menor que 
+  lte menor ou igual
+ */
+  let usuarios = await User.find({
+    age: { $lt: 30 },
+  });
+
   console.log("USUÁRIOS", usuarios);
 
   let age: number = 90;
